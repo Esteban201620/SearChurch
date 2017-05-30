89407f52613420c0c11886e73f39fa9c0d57700c
Esteban201620 <estebiosorio@gmail.com>
2017-05-30 12:04:04 -0500

    Commit 4

--- app/src/main/java/com/udistrital/marlonestebanosoriorico/searchurch/Ubicacion.java
+++ app/src/main/java/com/udistrital/marlonestebanosoriorico/searchurch/Ubicacion.java
@@ -7,6 +7,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -37,10 +38,16 @@ public class Ubicacion extends FragmentActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        UiSettings uisettings= mMap.getUiSettings();
+        uisettings.setZoomControlsEnabled(true);        // Add a marker in Sydney and move the camera
+        LatLng Bogota = new LatLng(4.6097100, -74.0817500);
+        float zoomlevel= 11;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        for (int i = 0; i< Controlador.iglesias.size(); i++) {
+            mMap.addMarker(new MarkerOptions().position(Controlador.iglesias.get(i).posicion).title(Controlador.iglesias.get(i).nombre));
+
+        }
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Bogota,zoomlevel));
     }
 }
